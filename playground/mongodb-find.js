1f8106bb19f5bb33e8cb3fc7870915998d9d1ba2
Leo Gonzalez <leo@circulo.digital>
2017-12-09 11:13:43 -0300

    first commit todo-app

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+	MongoClient,
+	ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) return console.log('Unable to connect to database. ', err);
+	console.log('Connected to MongoDB server');
+
+	// db.collection('Todos').find({
+	// 	_id: new ObjectID('5a293731bd9028cf0e8fe862'),
+	// }).toArray()
+	// .then((docs) => {
+	// 	console.log(JSON.stringify(docs, undefined, 2));
+	// })
+	// .catch((error) => {
+	// 	console.log('unable to fetch database ', error);
+	// });
+
+	// db.collection('Todos').find().count().then((count) => {
+	// 	console.log(`Todos count: ${count}`);
+	// }).catch((error) => {
+	// 	console.log('unable to fetch database ', error);
+	// });
+
+	db.collection('Users').find().toArray().then((docs) => {
+		console.log(JSON.stringify(docs, undefined, 2));
+	});
+
+	db.close();
+});
